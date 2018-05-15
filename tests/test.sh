a074347cdde087bfee5432892b80d28de69d4b76
gjr <gjr@gjr-mbp-2016.local>
2018-05-14 17:46:06 -0700

    add compare and tests

---
+++ tests/test.sh
@@ -0,0 +1,8 @@
+wget -c -nc https://docs.qiime2.org/2018.4/data/tutorials/moving-pictures/demux.qza
+
+qiime sourmash compute --i-sequence-file demux.qza --p-ksizes 21 --p-scaled 10000 --o-min-hash-signature sigs || { echo "compute fail"; rm demux.qza; exit 1; }
+
+qiime sourmash compare --i-min-hash-signature sigs.qza --p-ksize 21 --o-compare-output compare.mat || { echo "compare fail"; rm sigs.qza; exit 1; }
+
+echo "tests passed"
+rm -f sigs.qza compare.mat.qza demux.qza
