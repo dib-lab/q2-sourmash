4e8218d8bf2527a62914532e972f518dda36d652
gjr <guojiaro@gmail.com>
2018-05-14 18:03:51 -0700

    edit .gitignore and q2_sourmash/_compare.py

--- q2_sourmash/_compare.py
+++ q2_sourmash/_compare.py
@@ -10,16 +10,23 @@ from q2_types.distance_matrix import DistanceMatrix
 from q2_sourmash._format import MinHashSigJsonDirFormat
 import os
 import subprocess
+import numpy
+import skbio
 
-def compare(min_hash_signature:MinHashSigJsonDirFormat, ksize: int, ignore_abundance: bool=True) -> DistanceMatrix:
+def compare(min_hash_signature:MinHashSigJsonDirFormat, ksize: int, ignore_abundance: bool=True) -> skbio.DistanceMatrix:
 
-    output = DistanceMatrix
-    command = ['sourmash', 'compare', str(min_hash_signature) + "/*", '--ksize', str(ksize),
-    '-o', 'tmp']
-
+    np_file = 'tmp'
+    label_file = 'tmp.labels.txt'
+    command = ['sourmash', 'compare', str(min_hash_signature) + "/*", '--ksize', str(ksize), '-o', 'tmp']
     if ignore_abundance:
         command.append('--ignore-abundance')
-
     subprocess.run(' '.join(command), check=True, shell=True)
-
-    return output
+    # load np_file as np.ndarray -> np_sim
+    np_sim = numpy.load(np_file)
+    # convert similarity to distance
+    np_dis = 1 - np_sim
+    # read labels into a list -> labels
+    labels = [item.strip() for item in open(label_file)]
+    os.remove(np_file)
+    os.remove(label_file)
+    return skbio.DistanceMatrix(np_dis, labels)
