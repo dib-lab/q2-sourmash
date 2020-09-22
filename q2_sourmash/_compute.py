a01db3f7a26fb61025b7c44b0080f737bb3685cd
mikerobeson <soilbdelloid@gmail.com>
2020-09-22 17:11:41 -0500

    adding crude capability to handle genome fasta

--- q2_sourmash/_compute.py
+++ q2_sourmash/_compute.py
@@ -7,6 +7,7 @@
 # ----------------------------------------------------------------------------
 
 from q2_types.per_sample_sequences import SingleLanePerSampleSingleEndFastqDirFmt, FastqGzFormat
+from q2_types.feature_data import DNAIterator
 import qiime2.util
 import pandas as pd
 from q2_sourmash._format import MinHashSigJsonDirFormat
@@ -14,6 +15,41 @@ import os
 import subprocess
 import glob
 import sys
+import tempfile
+import shutil
+
+def compute_fasta(sequence_file: DNAIterator,
+                  ksizes: int,
+                  scaled: int,
+                  track_abundance: bool=True) -> MinHashSigJsonDirFormat:
+    output = MinHashSigJsonDirFormat()
+
+    # write files in tmp dir
+    #with tempfile.TemporaryDirectory() as temp_dir:
+    for seq in sequence_file:
+        tmp_fp = os.path.join(str(output), str(seq.metadata['id']) + '.fasta')
+        with open(tmp_fp, 'w') as indiv_fasta:
+            seq.write(indiv_fasta)
+
+    command = ['sourmash',
+               'compute',
+               str(output) + "/*",
+               '--ksizes',
+               str(ksizes),
+               '--scaled',
+               str(scaled)]
+
+    if track_abundance:
+        command.append('--track-abundance')
+
+    subprocess.run(' '.join(command), check=True, shell=True, cwd=str(output))
+
+    for seq_file in glob.glob(os.path.join(str(output), '*fasta')):
+        os.remove(seq_file)
+
+    sys.stdout.flush()
+
+    return output
 
 
 def compute(sequence_file:SingleLanePerSampleSingleEndFastqDirFmt, ksizes: int, scaled: int, track_abundance: bool=True) -> MinHashSigJsonDirFormat:
