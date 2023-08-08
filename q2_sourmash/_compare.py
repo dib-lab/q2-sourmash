f9e9e504ddbbabb77bcde2394bb85c1a8f1469d8
Michal Ziemski <mziemski@ethz.ch>
2023-08-08 16:08:28 +0200

    ENH: allow passing MAGs to the compute action

--- q2_sourmash/_compare.py
+++ q2_sourmash/_compare.py
@@ -5,28 +5,49 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
-from q2_types.distance_matrix import DistanceMatrix
-from q2_sourmash._format import MinHashSigJsonDirFormat
 import os
+import re
 import subprocess
+
 import numpy
 import skbio
 
-def compare(min_hash_signature:MinHashSigJsonDirFormat, ksize: int, ignore_abundance: bool=True) -> skbio.DistanceMatrix:
+from q2_sourmash._format import MinHashSigJsonDirFormat
+
+
+def compare(
+        min_hash_signature: MinHashSigJsonDirFormat,
+        ksize: int,
+        ignore_abundance: bool = True
+) -> skbio.DistanceMatrix:
 
     np_file = 'tmp'
     label_file = 'tmp.labels.txt'
-    command = ['sourmash', 'compare', str(min_hash_signature) + "/*", '--ksize', str(ksize), '-o', 'tmp']
+
+    command = [
+        'sourmash', 'compare', f'{min_hash_signature}/*',
+        '--ksize', str(ksize), '-o', 'tmp'
+    ]
     if ignore_abundance:
         command.append('--ignore-abundance')
+
     subprocess.run(' '.join(command), check=True, shell=True)
+
     # load np_file as np.ndarray -> np_sim
     np_sim = numpy.load(np_file)
+
     # convert similarity to distance
     np_dis = 1 - np_sim
+
     # read labels into a list -> labels
-    labels = [os.path.basename(filename).strip().strip('.fastq.gz') for filename in open(label_file)]
+    labels = [
+        re.sub(
+            r'\.(fastq\.gz|fasta)$', '', os.path.basename(filename).strip()
+        ) for filename in open(label_file)
+    ]
+
+    # clean up
     os.remove(np_file)
     os.remove(label_file)
+
     return skbio.DistanceMatrix(np_dis, labels)
--- q2_sourmash/_compute.py
+++ q2_sourmash/_compute.py
@@ -5,40 +5,77 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
-from q2_types.per_sample_sequences import SingleLanePerSampleSingleEndFastqDirFmt, FastqGzFormat
-import qiime2.util
-import pandas as pd
-from q2_sourmash._format import MinHashSigJsonDirFormat
+import glob
 import os
 import subprocess
-import glob
 import sys
+from typing import Union
+
+import pandas as pd
+import qiime2.util
+from q2_types.per_sample_sequences import SingleLanePerSampleSingleEndFastqDirFmt
+from q2_types_genomics.per_sample_data import MultiMAGSequencesDirFmt
+
+from q2_sourmash._format import MinHashSigJsonDirFormat
 
 
-def compute(sequence_file:SingleLanePerSampleSingleEndFastqDirFmt, ksizes: int, scaled: int, track_abundance: bool=True) -> MinHashSigJsonDirFormat:
-
-    #read in FastqManifestFormat to convert from sample name to filename
-    manifest = pd.read_csv(os.path.join(str(sequence_file), sequence_file.manifest.pathspec), header=0, comment='#')
-
+def _duplicate_mag_seqs(manifest, sequence_file):
     output = MinHashSigJsonDirFormat()
+    for i, row in manifest.iterrows():
+        sample_id, bin_id, src_fp = row["sample-id"], row["mag-id"], row["filename"]
+        src_fp = os.path.join(str(sequence_file), src_fp)
+        dest_fp = os.path.join(str(output), f'{bin_id}.fasta')
+        qiime2.util.duplicate(src_fp, dest_fp)
+    return output
 
-    for seq_file in glob.glob(os.path.join(str(sequence_file), '*fastq.gz')):
-        filepath = str(seq_file)
-        filename = os.path.basename(filepath)
-        sampleid = list(manifest[manifest['filename']==filename]['sample-id'])
-        # print(sampleid, filename)
-        qiime2.util.duplicate(filepath, os.path.join(str(output), sampleid[0]+'.fastq.gz'))
 
-    command = ['sourmash', 'compute', str(output) + "/*", '--ksizes', str(ksizes), '--scaled', str(scaled)]
+def _duplicate_seqs(manifest, sequence_file):
+    output = MinHashSigJsonDirFormat()
+    for seq_file in glob.glob(os.path.join(str(sequence_file), '*.fastq.gz')):
+        src_fp = str(seq_file)
+        filename = os.path.basename(src_fp)
+        sample_id = list(manifest[manifest['filename'] == filename]['sample-id'])
+        dest_fp = os.path.join(str(output), f'{sample_id[0]}.fastq.gz')
+        qiime2.util.duplicate(src_fp, dest_fp)
+    return output
+
+
+def compute(
+        sequence_file: Union[
+            SingleLanePerSampleSingleEndFastqDirFmt, MultiMAGSequencesDirFmt
+        ],
+        ksizes: int,
+        scaled: int,
+        track_abundance: bool = True
+) -> MinHashSigJsonDirFormat:
+    # read in FastqManifestFormat to convert from sample name to filename
+    manifest = pd.read_csv(
+        os.path.join(str(sequence_file), sequence_file.manifest.pathspec),
+        header=0, comment='#'
+    )
+
+    if isinstance(sequence_file, MultiMAGSequencesDirFmt):
+        output = _duplicate_mag_seqs(manifest, sequence_file)
+    else:
+        output = _duplicate_seqs(manifest, sequence_file)
+
+    command = [
+        'sourmash', 'compute', f'{output}/*',
+        '--ksizes', str(ksizes),
+        '--scaled', str(scaled)
+    ]
 
     if track_abundance:
         command.append('--track-abundance')
 
     subprocess.run(' '.join(command), check=True, shell=True, cwd=str(output))
 
-    for seq_file in glob.glob(os.path.join(str(output), '*fastq.gz')):
-        os.remove(seq_file)
+    if isinstance(sequence_file, MultiMAGSequencesDirFmt):
+        ext = '*.fasta'
+    else:
+        ext = '*.fastq.gz'
+    for fp in glob.glob(os.path.join(str(output), ext)):
+        os.remove(fp)
 
     sys.stdout.flush()
 
